e248ecdc81f7c1c1c80efe1311c15114f67f9ecb
Megan Hart <meganhart4242@gmail.com>
2018-04-17 20:36:23 -0500

    added date to singleposts trying to fix database pull/object issues, broken line 77

--- main.py
+++ main.py
@@ -49,18 +49,18 @@ app.secret_key = "dsjfhirwbrguakdbufbuwe"
 def get_old_blogs():
     return Blog.query.all()
 
+def get_users():
+    return User.query.all()
+
 @app.before_request
 def require_login():  
-        #allowed_routes 'login', 'blog', 'index', 'signup' #these are the function names NOT the handler names
-        #request.endpoint   is the name of the function not the url path
-        #if the user is trying to go anywhere else, and not logged in    ....if user not in session:
     allowed_routes = ['login', 'blog', 'index', 'signup']
     if request.endpoint not in allowed_routes and 'username' not in session:
         return redirect('/login')
            
 @app.route('/')
-def index(): 
-    return render_template("index.html", old_posts = get_old_blogs())
+def index():         #need to fix database syntax to show authors on index - posibly working now, test in a minute
+    return render_template("index.html", old_posts = get_old_blogs(), user = get_users())
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
@@ -72,18 +72,21 @@ def login():
         password = request.form["password"]
 
     #I need to query the database  using the user name and password they submitted.   
-        user_names = get_old_blogs() #this line needs to get the entire database of user-names
+        user_names = get_users() #this line needs to get the entire database of user-names
+        for user in user_names:
+#I think I need a try/except block here. 
 
-        if username not in user_names.username:
-            #return the flash message "This user name does not exist"
-            flash("This user name does not exist")
-            if password not in user_names.password:
-            #return flash message "Password is incorrect"
-                flash("Password is incorrect")
-        if username == user_names.username and password == user_names.password:
-        #TODO store their user name in the session 
-            session['username'] = username
-            return redirect("/newpost")
+
+            if username not in user_names:
+                #return the flash message "This user name does not exist"
+                flash("This user name does not exist")
+                if password not in user_names.password:
+                #return flash message "Password is incorrect"
+                    flash("Password is incorrect")
+            if username == user_names.username and password == user_names.password:
+            #TODO store their user name in the session 
+                session['username'] = username
+                return redirect("/newpost")
 
 
 # signup handler
@@ -160,6 +163,8 @@ def blog():
     if request.method == 'POST': #you have submitted a form validate it. If good save and render single post
         title = request.form['title']
         body = request.form['body']
+        username = request.form['username']
+        single_user = User.query.get(username).first
         title_error=""
         body_error=""
         if body == "                ":  #this line isn't working, it did, then I changed it to a textarea. 
@@ -168,13 +173,13 @@ def blog():
             title_error = "Please remember to enter a title." 
            
         if not title_error and not body_error :  #this passes if the strings stay empty
-            blog = Blog(title, body)
+            blog = Blog(title, body, single_user.id)
             db.session.add(blog)
             db.session.commit()  
-            after_submit = "/blog?id=" + str(blog.id)    #stored here, not using it
+            after_submit = "/blog?id=" + str(blog.id)    
             return redirect(after_submit)
 
-        else:  #it had an error
+        else:                                   #it had an error
             return render_template("newpost.html",
                 title=title,
                 body=body,
@@ -185,14 +190,16 @@ def blog():
     if request.method =="GET":
         if request.args.get('id'): #how to show single posts from get request
             blog_id = request.args.get('id')
-            blog = Blog.query.get(blog_id)
+            blog = get_old_blogs()
             return render_template("singlepost.html",
             head_title="My blog", 
-            blog = blog)        #TypeError: 'Blog' object is not iterable
+            blog = blog)        #TypeError: 'Blog' object is not iterable    .sort_by("date desc")
 
         if request.args.get('user'): #how to show single posts from a single author get request
             user_id = request.args.get('user')
-            blog = Blog.query.get(user_id)  #this line should get all of the posts from that user_id
+            blog = Blog.query.get().all.sort_by("date desc")
+            
+             #this line should get all of the posts from that user_id
             #is there away right here to sort by date as the blog object is created?
             return render_template("singlepost.html",
             head_title="My blog", 
@@ -205,7 +212,9 @@ def blog():
 
 @app.route('/newpost', methods=['POST', 'GET'])
 def newpost():
-    return render_template('newpost.html', head_title="New Post")
+# so, I need some way to add/commit the owner_id with these posts. 
+    username = session['username']    #can I get the current username out? 
+    return render_template('newpost.html', head_title="New Post", username = username)
     
 
 
